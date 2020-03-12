745e5b232a5b50b4c9dff5dd468b7c5d1af8e0af
Javier <javier@silver.io>
2020-03-12 01:38:33 +0000

    adds a loop to pick the Pokémon names, also a few visual changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 var repository = [
-    {name: 'Fearow', height: 1.2, types: ['flying', 'normal']},
-    {name: 'Gible', height: 0.7, types: ['dragon', 'ground']},
-    {name: 'Graveler', height: 1, types: ['rock', 'ground'] }
-];
+    {
+        name: 'Fearow', 
+        height: 1.2, 
+        types: ['flying', 'normal']
+    },
+    {
+        name: 'Gible', 
+        height: 0.7, 
+        types: ['dragon', 'ground']
+    },
+    {
+        name: 'Graveler', 
+        height: 1, 
+        types: ['rock', 'ground'] 
+}];
+
+for (var i = 0; i < repository.length; i++) {
+    document.write("<p>" + repository[i].name + ' (Height: ' + repository[i].height + ')' + "</p>")
+    if (repository[i].height > 1) {
+         document.write("<p>" + 'The tallest Pokémon on this list!' + "</p>")
+    }
+  }
